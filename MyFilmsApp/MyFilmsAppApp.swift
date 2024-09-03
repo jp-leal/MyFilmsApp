54aa1a9c3554d0c5181a618c8c4aa6b59dfd04da
joaobug <joaoenfrance@gmail.com>
2024-09-03 09:39:35 -0300

    Initial Commit

---
+++ MyFilmsApp/MyFilmsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFilmsAppApp.swift
+//  MyFilmsApp
+//
+//  Created by Joao Leal on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFilmsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
